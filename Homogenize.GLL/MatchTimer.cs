6106cd89dacf5b141f42b0c42cd54f4714db2c10
Victor Shin Kinoshita <nct.shin@gmail.com>
2013-08-24 16:22:55 -0300

    MatchTimer

---
+++ Homogenize.GLL/MatchTimer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Timers;
+
+namespace Homogenize.GLL
+{
+    public class MatchTimer
+    {
+        private DateTime startTime;
+        private DateTime endTime;
+
+        public TimeSpan CurrentTimeSpan
+        {
+            get 
+            {
+                if (DateTime.Now - startTime > MaxMatchTimeSpan)
+                {
+                    Stop();
+                    endTime = startTime.Add(MaxMatchTimeSpan);
+                    IsTimerAlive = false;
+                    return MaxMatchTimeSpan;
+                }
+                else if (IsTimerAlive)
+                {
+                    return DateTime.Now - startTime;
+                }
+                else
+                {
+                    return endTime - startTime;
+                }
+            }
+        }
+        public bool IsTimerAlive { get; private set; }
+        public TimeSpan MaxMatchTimeSpan { get; set; }
+
+        public MatchTimer()
+        {
+            MaxMatchTimeSpan = TimeSpan.MaxValue;
+        }
+
+        public void Start()
+        {
+            IsTimerAlive = true;
+            startTime = DateTime.Now;
+        }
+        public void Stop()
+        {
+            IsTimerAlive = false;
+            endTime = DateTime.Now;
+        }
+    }
+}
